29ebcfc9ffb640d918692e4dc47a18553ec4ac11
samir-bueno <samirbueno585@gmail.com>
2025-04-26 23:25:38 -0300

    cree una carpeta de hojas-de-estilo y para crear los estilos de los futuros componentes, ademas cree una de imagenes, y modifique el app.jsx para los demas componentes

--- pagina_artesanal/src/App.jsx
+++ pagina_artesanal/src/App.jsx
@@ -1,9 +1,32 @@
-function App() {
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
+// Componentes
+import Header from "./components/Header"
+import Footer from "./components/Footer"
+import Home from "./pages/Home"
+import Products from "./pages/Products"
+import AboutUs from "./pages/AboutUs"
+import ScrollToTop from "./components/ScrollToTop"
+
+// Estilos
+import "./hojas-de-estilo/App.css"
+
+function App() {
   return (
-    <>
-  
-    </>
+    <Router>
+      <ScrollToTop />
+      <div className="app">
+        <Header />
+        <main className="main-content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/productos" element={<Products />} />
+            <Route path="/sobre-nosotros" element={<AboutUs />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
   )
 }
 
